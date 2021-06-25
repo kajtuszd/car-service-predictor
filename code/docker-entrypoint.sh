321a6de71e92df3b00c4759fe0f078a23aeea0a4
kajetan <kajetanzdanowicz@wp.pl>
2021-06-25 17:03:13 +0200

    docker containers, requirements and django project

---
+++ code/docker-entrypoint.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+ownership() {
+    # Fixes files ownership
+    # source: https://github.com/BD2KGenomics/cgl-docker-lib/blob/master/mutect/runtime/wrapper.sh#L5
+    user_id=$(stat -c '%u:%g' /code)
+    chown -R ${user_id} /code
+}
+
+echo ''
+echo '--------------------------'
+echo 'Install missing packages'
+echo '--------------------------'
+echo ''
+pip install -r ./requirements.txt
+
+echo ''
+echo '--------------------------'
+echo 'Database migration'
+echo '--------------------------'
+echo ''
+
+bash wait_for_db.sh
+
+python manage.py makemigrations || exit 1
+python manage.py migrate || exit 1
+
+echo ''
+echo '--------------------------'
+echo 'Ownership fixes'
+echo '--------------------------'
+echo ''
+ownership
+
+echo ''
+echo '-------------------------'
+echo 'Run command'
+echo $@
+echo '-------------------------'
+echo ''
+python manage.py $@ || exit 1
---
+++ code/wait_for_db.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for PostgreSQL..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
