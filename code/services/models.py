dc387a64f7d60bb6be61edca839f94bb7f36784e
kajetan <kajetanzdanowicz@wp.pl>
2021-07-31 00:15:58 +0200

    service admin

--- code/services/models.py
+++ code/services/models.py
@@ -7,11 +7,14 @@ from django.utils.translation import ugettext_lazy as _
 
 class Service(models.Model):
     title = models.CharField(_('Title'), max_length=30)
-    cost = models.DecimalField(_('Cost'), validators=[MaxValueValidator(50000), MinValueValidator(0)],
-                                   decimal_places=2, max_digits=7)
+    cost = models.DecimalField(_('Cost'), validators=[MaxValueValidator(50000),
+                                                      MinValueValidator(0)],
+                               decimal_places=2, max_digits=7)
     car_part = models.ForeignKey(CarPart, on_delete=models.CASCADE)
-    date_start = models.DateTimeField(_('Start date'), validators=[no_past_validator])
-    date_finish = models.DateTimeField(_('Finish date'), validators=[no_future_validator])
+    date_start = models.DateTimeField(_('Start date'),
+                                      validators=[no_past_validator])
+    date_finish = models.DateTimeField(_('Finish date'),
+                                       validators=[no_future_validator])
     is_active = models.BooleanField(_('Is active'), default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
