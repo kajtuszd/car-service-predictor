8b09c6f2a6023a975edacb9a68d6ee00c620c75e
kajetan <kajetanzdanowicz@wp.pl>
2021-09-05 18:01:08 +0200

    car app urls & viewsets, users app api fixes

---
+++ code/users/urls.py
@@ -0,0 +1,11 @@
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
+router = DefaultRouter()
+router.register(r'user', viewset=views.UserViewSet, basename='user')
+router.register(r'customer', viewset=views.CustomerViewSet, basename='customer')
+router.register(r'workshop', viewset=views.WorkshopViewSet, basename='workshop')
+
+urlpatterns = [
+] + router.urls
