e3c3985b44b2e0bb22babbfcff6566005ba8202d
kajetan <kajetanzdanowicz@wp.pl>
2021-09-07 11:11:43 +0200

    make it better

--- front/src/main.js
+++ front/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import axios from 'axios'
 
 Vue.config.productionTip = false
+
+// axios.defaults.baseURL = 'http://0.0.0.0:8000/'
 
 new Vue({
   render: h => h(App),
