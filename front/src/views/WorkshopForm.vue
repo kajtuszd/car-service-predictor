13f1be5395c1402cd10552b711395dda2f66c619
kajetan <kajetanzdanowicz@wp.pl>
2021-10-03 16:52:28 +0200

    workshop form for new data

--- front/src/views/WorkshopForm.vue
+++ front/src/views/WorkshopForm.vue
@@ -15,6 +15,57 @@
                         </div>
                     </div>
 
+                    <div class="field">
+                        <label>City</label>
+                        <div class="control has-icons-left">
+                            <input type="text" name="city" class="input" v-model="city">
+                            <span class="icon is-small is-left">
+                                <i class="fas fa-city"></i>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div class="field">
+                        <label>Street</label>
+                        <div class="control has-icons-left">
+                            <input type="text" name="street" class="input" v-model="street">
+                            <span class="icon is-small is-left">
+                                <i class="fas fa-road"></i>
+                            </span>
+                        </div>
+                    </div>
+
+
+                    <div class="field">
+                        <label>House number</label>
+                        <div class="control has-icons-left">
+                            <input type="text" name="house_number" class="input" v-model="house_number">
+                            <span class="icon is-small is-left">
+                                <i class="fas fa-home"></i>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div class="field">
+                        <label>Flat number</label>
+                        <div class="control has-icons-left">
+                            <input type="text" name="flat_number" class="input" v-model="flat_number">
+                            <span class="icon is-small is-left">
+                                <i class="fas fa-home"></i>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div class="field">
+                        <label>Zip code</label>
+                        <div class="control has-icons-left">
+                            <input type="text" name="zip_code" class="input" v-model="zip_code">
+                            <span class="icon is-small is-left">
+                                <i class="fas fa-sort-numeric-up-alt"></i>
+                            </span>
+                        </div>
+                    </div>
+
                     <div class="notification is-danger" v-if="errors.length">
                         <button class="delete" v-on:click="cleanErrors"></button>
                         <p v-for="e in errors" :key="e">{{e}}</p>
@@ -45,13 +96,35 @@
                     this.errors.push('Workshop name is required.')
                 }
 
+                if (this.city === '') {
+                    this.errors.push('City is required.')
+                }
+
+                if (this.street === '') {
+                    this.errors.push('Street is required.')
+                }
+
+                if (this.house_number === '') {
+                    this.errors.push('House number is required.')
+                }
+
+                if (this.zip_code === '') {
+                    this.errors.push('Zip code is required.')
+                }
+
                 if (!this.errors.length) {
-                    const workshopName = {
+                    const workshopData = {
                         workshop_name: this.workshopName,
+                        city: this.city,
+                        street: this.street,
+                        house_number: this.house_number,
+                        flat_number: this.flat_number,
+                        zip_code: this.zip_code,
+
                     }
 
                     const workshop = {
-                        workshop: workshopName,
+                        workshop: workshopData,
                     }
 
                     const usernameStored = localStorage.getItem('username')
