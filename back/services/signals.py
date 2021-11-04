80c77057f9b7c87002538a6adf1df91bf6187677
kajetan <kajetanzdanowicz@wp.pl>
2021-11-04 13:02:31 +0100

    delta time service task fix

--- back/services/signals.py
+++ back/services/signals.py
@@ -10,5 +10,10 @@ from .tasks import update_service
 @receiver(pre_save, sender=Service)
 def set_up_service_task(instance, **kwargs):
     d_seconds = instance.time.second - datetime.now().second
-    service_datetime = datetime.utcnow() + timedelta(seconds=d_seconds)
+    d_minutes = instance.time.minute - datetime.now().minute
+    d_hours = instance.time.hour - datetime.now().hour
+    d_date = instance.date - datetime.now().date()
+    service_datetime = datetime.utcnow() + timedelta(
+        seconds=d_seconds, minutes=d_minutes, hours=d_hours, days=d_date.days
+        )
     update_service.apply_async(eta=service_datetime)
