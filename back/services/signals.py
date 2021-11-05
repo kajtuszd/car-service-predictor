6acddc502ce9130abd4b553a10e5abc96e15cbf9
kajetan <kajetanzdanowicz@wp.pl>
2021-11-05 11:19:24 +0100

    service celery task

--- back/services/signals.py
+++ back/services/signals.py
@@ -1,19 +1,22 @@
 from datetime import datetime, timedelta
 
-from django.db.models.signals import pre_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from .models import Service
 from .tasks import update_service
 
 
-@receiver(pre_save, sender=Service)
+@receiver(post_save, sender=Service)
 def set_up_service_task(instance, **kwargs):
-    d_seconds = instance.time.second - datetime.now().second
-    d_minutes = instance.time.minute - datetime.now().minute
-    d_hours = instance.time.hour - datetime.now().hour
-    d_date = instance.date - datetime.now().date()
-    service_datetime = datetime.utcnow() + timedelta(
-        seconds=d_seconds, minutes=d_minutes, hours=d_hours, days=d_date.days
-        )
-    update_service.apply_async(eta=service_datetime)
+    if instance.is_active:
+        d_seconds = instance.time.second - datetime.now().second
+        d_minutes = instance.time.minute - datetime.now().minute
+        d_hours = instance.time.hour - datetime.now().hour
+        d_date = instance.date - datetime.now().date()
+        service_datetime = datetime.utcnow() + timedelta(seconds=d_seconds,
+                                                        minutes=d_minutes,
+                                                        hours=d_hours,
+                                                        days=d_date.days)
+        update_service.apply_async(eta=service_datetime,
+                                kwargs={'slug': instance.slug})
--- back/services/tasks.py
+++ back/services/tasks.py
@@ -1,11 +1,35 @@
 from __future__ import absolute_import, unicode_literals
-from datetime import timedelta, date
+
+from datetime import date, timedelta
 
 from celery import shared_task
-from cars.models import CarPart, Car
+
 from .models import Service
 
 
+def calculate_next_fix_date(car_part):
+    car_part.next_fix_mileage = car_part.car.mileage + car_part.fix_every_mileage
+    next_service_days = car_part.next_fix_mileage / car_part.car.daily_mileage
+    next_fix_date1 = date.today() + timedelta(days=next_service_days)
+    next_fix_date2 = car_part.latest_fix_date + timedelta(
+        days=car_part.fix_every_period)
+    if next_fix_date1 < car_part.next_fix_date or next_fix_date2 < car_part.next_fix_date:
+        car_part.next_fix_date = date.today()
+    else:
+        if next_fix_date1 < next_fix_date2:
+            car_part.next_fix_date = next_fix_date1
+        else:
+            car_part.next_fix_date = next_fix_date2
+    car_part.car.save()
+    car_part.save()
+
+
 @shared_task
-def update_service():
-    print("hello")
+def update_service(slug):
+    service = Service.objects.get(slug=slug)
+    service.car_part.latest_fix_date = service.date
+    service.car_part.latest_fix_mileage = service.car_part.car.mileage
+    service.is_active = False
+    service.car_part.save()
+    calculate_next_fix_date(service.car_part)
+    service.save()
