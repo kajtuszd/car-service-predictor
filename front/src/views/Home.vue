3741fbbcee28ed92fb118cbbc6b9ca52267f83ea
kajetan <kajetanzdanowicz@wp.pl>
2021-12-17 02:53:47 +0100

    all objects stats

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -6,17 +6,19 @@
                 <div class="tile">
                     <div class="tile is-parent is-vertical">
                         <article class="tile is-10 is-child notification is-primary">
-                            <p class="title">All cars</p>
+                            <p class="title">Cars</p>
                             <p class="subtitle">registered in system</p>
                             <h1 class="title">{{this.allCars}}</h1>
                         </article>
                         <article class="tile is-10 is-child notification is-primary">
-                            <p class="title">...tiles</p>
-                            <p class="subtitle">Bottom tile</p>
+                            <p class="title">Car parts</p>
+                            <p class="subtitle">registered in system</p>
+                            <h1 class="title">{{this.allCarParts}}</h1>
                         </article>
                         <article class="tile is-10 is-child notification is-primary">
-                            <p class="title">...tiles</p>
-                            <p class="subtitle">Bottom tile</p>
+                            <p class="title">Services</p>
+                            <p class="subtitle">registered in system</p>
+                            <h1 class="title">{{this.allServices}}</h1>
                         </article>
                     </div>
                 </div>
@@ -26,12 +28,14 @@
                 <div class="tile">
                     <div class="tile is-parent is-vertical">
                         <article class="tile is-10 is-child notification is-primary">
-                            <p class="title">Vertical...</p>
-                            <p class="subtitle">Top tile</p>
+                            <p class="title">Users</p>
+                            <p class="subtitle">registered in system</p>
+                            <h1 class="title">{{this.allUsers}}</h1>
                         </article>
                         <article class="tile is-10 is-child notification is-primary">
-                            <p class="title">...tiles</p>
-                            <p class="subtitle">Bottom tile</p>
+                            <p class="title">Workshops</p>
+                            <p class="subtitle">registered in system</p>
+                            <h1 class="title">{{this.allWorkshops}}</h1>
                         </article>
                         <article class="tile is-10 is-child notification is-primary">
                             <p class="title">...tiles</p>
@@ -71,6 +75,10 @@
         name: 'Home',
         mounted() {
             this.getAllCarsNumber()
+            this.getAllCarPartsNumber()
+            this.getAllServicesNumber()
+            this.getAllUsersNumber()
+            this.getAllWorkshopsNumber()
         },
         methods: {
             async getAllCarsNumber() {
@@ -79,11 +87,43 @@
                     .then(response => {
                         this.allCars = response.data
                     })
+            },
+            async getAllCarPartsNumber() {
+                await axios
+                    .get('cars/car-part/all/')
+                    .then(response => {
+                        this.allCarParts = response.data
+                    })
+            },
+            async getAllServicesNumber() {
+                await axios
+                    .get('services/service/all/')
+                    .then(response => {
+                        this.allServices = response.data
+                    })
+            },
+            async getAllUsersNumber() {
+                await axios
+                    .get('users/user/all/')
+                    .then(response => {
+                        this.allUsers = response.data
+                    })
+            },
+            async getAllWorkshopsNumber() {
+                await axios
+                    .get('users/workshop/all/')
+                    .then(response => {
+                        this.allWorkshops = response.data
+                    })
             }
         },
         data() {
             return {
                 allCars: '',
+                allCarParts: '',
+                allWorkshops: '',
+                allUsers: '',
+                allServices: '',
             }
         }
     }
